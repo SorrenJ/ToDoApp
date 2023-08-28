0e9ce6ea5c6ba039bbf25b71f21d3e7a99504931
Sorren Jao <soalexjao@gmail.com>
2023-08-28 13:05:34 -0700

    searchbar

--- TodoApp/lib/home.dart
+++ TodoApp/lib/home.dart
@@ -10,38 +10,78 @@ class Home extends StatelessWidget{
 
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-elevation:0,
-        title: Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children:[
-          Icon(
-            Icons.menu,
-            color: tdBlack,
-            size: 30,
-      ),
-
-      Container(
-
-        height: 40,
-        width: 40,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.asset('images/avatar.jpeg'),
-        ),
-      ),
-  ]),
-      ),
+      backgroundColor: Colors.grey,
+      appBar: buildAppBar(context),
   body: Container (
-
-    child:Text('This is my home'),
+padding: EdgeInsets.symmetric(
+    horizontal: 20,
+    vertical: 15
+),
+    child:Column(
+      children: [
+       searchBox(),
+  ],
     ),
-  );
+    ),
+    );
 
 
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20)
+        ),
+        child:TextField(
+
+          decoration:InputDecoration(
+            contentPadding: EdgeInsets.all(3),
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints (
+                maxHeight: 20,
+                minWidth: 25
+            ),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: Colors.grey),
+          ),
+        ),
+
+      );
+
+  }
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+elevation:0,
+      title: Row(
+mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:[
+        Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+    ),
+
+    Container(
+
+      height: 40,
+      width: 40,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset('images/avatar.jpeg'),
+      ),
+    ),
+]),
+    );
   }
 }
 
