c8d7bf47d721461b1c0228b5778e2cb4b89b2c47
Sorren Jao <soalexjao@gmail.com>
2023-08-30 23:00:53 -0700

    Completed static look

--- TodoApp/lib/home.dart
+++ TodoApp/lib/home.dart
@@ -14,7 +14,9 @@ class Home extends StatelessWidget{
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: buildAppBar(context),
-  body: Container (
+  body: Stack(
+  children: [
+  Container(
 padding: EdgeInsets.symmetric(
     horizontal: 20,
     vertical: 15
@@ -39,9 +41,52 @@ padding: EdgeInsets.symmetric(
       ],
     ),
     ),
+Align(
+  alignment:Alignment.bottomCenter,
+  child:Row(children:[
+    Expanded(child: Container(margin: EdgeInsets.only(bottom: 20, right: 20, left: 20,
+    ),
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5, ),
+        decoration: BoxDecoration(
+      color: Colors.white,
+      boxShadow: const [BoxShadow(
+        color: Colors.grey,
+        offset: Offset(0.0,0.0),
+      blurRadius:10.0,
+        spreadRadius:0.0,
+      ),],
+   borderRadius: BorderRadius.circular(10),
+
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText:'Add a new todo item',
+        border: InputBorder.none
+
+      ),
+    ),
+    ),
+    ),
+  Container(
+    margin:EdgeInsets.only(bottom: 20,
+        right: 20
+    ),
+  child: ElevatedButton(
+    child:Text('+',style: TextStyle(fontSize: 40,),),
+    onPressed: () {},
+    style: ElevatedButton.styleFrom(
+      primary: Colors.blue,
+      minimumSize: Size(60,60),
+      elevation: 10,
+    )
+  )
+  )
+  ])
+),
+
+    ],
+  ),
     );
-
-
   }
 
   Widget searchBox() {
